0d914aec1ae97ccf541e7df2c3697dcbb92ee4a8
j-frank879 <j.frankowska8844@gmail.com>
2021-04-27 16:47:54 +0200

    szkielet aplikacji

---
+++ Projekt_SI/src/projekt_si/Dane.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekt_si;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Joanna
+ */
+public class Dane {
+  //0-NKB
+  //1-Gray 
+  int kod=-1;
+ //0-krzyzowanie
+  //1-mutacja 
+  int w=-1;
+
+  
+  
+
+    
+}
