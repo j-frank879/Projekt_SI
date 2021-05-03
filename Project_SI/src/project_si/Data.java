7967ddfc0163bdb6d257e000d3e85f8d2529b8fe
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-05-03 10:31:09 +0200

    name of package translated

--- Project_SI/src/project_si/Data.java
+++ Project_SI/src/project_si/Data.java
@@ -1,4 +1,4 @@
-package projekt_si;
+package project_si;
 
 import java.util.ArrayList;
 import java.util.List;
--- Project_SI/src/project_si/Main_Frame.java
+++ Project_SI/src/project_si/Main_Frame.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package projekt_si;
+package project_si;
 
 import java.awt.Dimension;
 
