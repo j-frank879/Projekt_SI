4f925a2ed08338470e9befa57a1a6f69fc127a55
bdaf <kamilkorenkiewiczpw@gmail.com>
2021-05-17 17:13:13 +0200

    make Data little more readable

--- model/src/main/java/project_si/Data.java
+++ model/src/main/java/project_si/Data.java
@@ -7,28 +7,21 @@ import java.util.Random;
 public class Data {
     //0-NKB
     //1-Gray
-    Coding code = Coding.NKB;
+    private Coding kindOfCoding;
+    private int kindOfCrossing;
+    private double probabilityOfCrossover;
+    private double probabilityOfMutation;
+    private int sizeOfPopulation;
+    private String twoVariablesFunction;
+    private int minX = 0, maxX = 0, minY = 0, maxY = 0;
+    int numberOfGeneration;
 
-    //kind of crossing
-    int kindOfCrossing = -1;
-
-    //probability of crossover mutation
-    double crossover = 0;
-    double mutation = 0;
-
-    //population size
-    int size_population = 0;
+    //length of binary word example 01101 bin_lenght=5
+    int lengthOfBinaryWord;
 
     //population saved in code
     ArrayList<Individual> dane = new ArrayList<>();
 
-    // the adaptation function
-    String function="";
-    int minX = 0, maxX = 0, minY = 0, maxY = 0;
-    //number of generatons
-    int generation = 1;
-    //length of binary word example 01101 bin_lenght=5
-    int bin_length = 0;
 
     void set_bin_length() {
         int a = (maxX > maxY) ? maxX : maxY;
@@ -38,12 +31,12 @@ public class Data {
             x++;
         }
         x++;
-        bin_length = x;
+        lengthOfBinaryWord = x;
     }
 
     String decimal_to_binary(int x) {
         String s = Integer.toBinaryString(x);
-        while ((s.length() < bin_length)) {
+        while ((s.length() < lengthOfBinaryWord)) {
             s = "0" + s;
         }
         return s;
@@ -93,82 +86,83 @@ public class Data {
         return a;
     }
 
-	//calculating if crossover/mutation happened 
-  Boolean probability(double x)
-  {int q=1;
-	  while(x%1>0)
-	  {x*=10;
-  q*=10;}
-  Random rand = new Random();
-  int r=rand.nextInt(q);
-  if(r<x)
-  {return true;}
-  else
-	  return false;
-	  
-  }
-  //generating first generation in dane list
-  void first_gen()
-  {set_bin_length();
-  dane.clear();
-  Individual next=new Individual();
-	  for(int i=minX;i<maxX;i++)
-	  {
-		  for(int j=minY;j<maxY;j++)
-		  {next.x=i;
-	  next.y=j;
-	  if(code== Coding.NKB)
-	  next.in=decimal_to_binary(i)+decimal_to_binary(j);
-  else
-	  next.in=binary_to_gray(decimal_to_binary(i))+binary_to_gray(decimal_to_binary(j));
-		
-next.adaptation();
-dane.add(new Individual(next));		
-		  }
-	  }
-  
-	  
-  }
-  
-  void onepoint_crossover()
-  {ArrayList<Individual> new_gen = new ArrayList<>();
-  
-  Individual next=new Individual();
-  //crossing 
-  int s=bin_length*2-1;
-  int i=-1;
-  int j=-1;
-  
-  for(Individual ind1:dane)
-  {i++;
-	for(Individual ind2:dane)
-  {j++;
-	if((j<i)||(!(probability(crossover))))
-		continue;
-	//first
-	next.in=ind1.in.substring(0,bin_length-1)+ind2.in.substring(bin_length,s);
-	 next.x=ind1.x;
-	 next.y=ind2.y;
-	 next.adaptation();
-	 new_gen.add(new Individual(next));
-	 //second
-	 next.in=ind2.in.substring(0,bin_length-1)+ind1.in.substring(bin_length,s);
-	 next.x=ind2.x;
-	 next.y=ind1.y;
-	 next.adaptation();
-	 new_gen.add(new Individual(next));
-	  
-  }
-	  j=-1;
-  }
-  for(Individual x:new_gen)
-  {dane.add(x);
-	  
-  }
-  }
-	
-    //rulet selection
-    void Selection() {
+    //calculating if crossover/mutation happened
+    Boolean probability(double x) {
+        int q = 1;
+        while (x % 1 > 0) {
+            x *= 10;
+            q *= 10;
+        }
+        Random rand = new Random();
+        int r = rand.nextInt(q);
+        if (r < x) {
+            return true;
+        } else
+            return false;
+
+    }
+
+    //generating first generation in dane list
+    void makeFirstGeneration() {
+        set_bin_length();
+        dane.clear();
+        Individual next = new Individual();
+        for (int i = minX; i < maxX; i++) {
+            for (int j = minY; j < maxY; j++) {
+                next.x = i;
+                next.y = j;
+                if (kindOfCoding == Coding.NKB)
+                    next.in = decimal_to_binary(i) + decimal_to_binary(j);
+                else
+                    next.in = binary_to_gray(decimal_to_binary(i)) + binary_to_gray(decimal_to_binary(j));
+
+                next.adaptation();
+                dane.add(new Individual(next));
+            }
+        }
+
+
+    }
+
+    void onePointCrossover() {
+        ArrayList<Individual> new_gen = new ArrayList<>();
+
+        Individual next = new Individual();
+        //crossing
+        int s = lengthOfBinaryWord * 2 - 1;
+        int i = -1;
+        int j = -1;
+
+        for (Individual ind1 : dane) {
+            i++;
+            for (Individual ind2 : dane) {
+                j++;
+                if ((j < i) || (!(probability(probabilityOfCrossover))))
+                    continue;
+                //first
+                next.in = ind1.in.substring(0, lengthOfBinaryWord - 1) + ind2.in.substring(lengthOfBinaryWord, s);
+                next.x = ind1.x;
+                next.y = ind2.y;
+                next.adaptation();
+                new_gen.add(new Individual(next));
+                //second
+                next.in = ind2.in.substring(0, lengthOfBinaryWord - 1) + ind1.in.substring(lengthOfBinaryWord, s);
+                next.x = ind2.x;
+                next.y = ind1.y;
+                next.adaptation();
+                new_gen.add(new Individual(next));
+
+            }
+            j = -1;
+        }
+        for (Individual x : new_gen) {
+            dane.add(x);
+
+        }
+    }
+
+    //roulette  selection
+    void rouletteSelection() {
         Random rand = new Random();
 
         int dl = dane.size();
@@ -189,7 +183,7 @@ dane.add(new Individual(next));
         }
         i = 0;
         int r, l = 0, h = dl - 1, mid, index;
-        while (i < size_population) {
+        while (i < sizeOfPopulation) {
             r = rand.nextInt(prefix[dl - 1]) + 1;
 
 
@@ -211,8 +205,7 @@ dane.add(new Individual(next));
             i++;
         }
 
-
-        while (dane.size() > size_population) {
+        while (dane.size() > sizeOfPopulation) {
             dane.remove(0);
 
         }
